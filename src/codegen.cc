57ad91be1df61c3b53e1a4b7ac6b04d493c972cb
days0102 <411883104@qq.com>
2022-11-22 16:01:17 +0800

    rebuild

--- src/codegen.cc
+++ src/codegen.cc
@@ -2,13 +2,94 @@
  * @Author: Outsider
  * @Date: 2022-10-31 21:28:22
  * @LastEditors: Outsider
- * @LastEditTime: 2022-11-02 09:54:31
+ * @LastEditTime: 2022-11-22 16:00:14
  * @Description: In User Settings Edit
  * @FilePath: /compiler/src/codegen.cc
  */
 #include "codegen.hh"
+#include "tree.hh"
 
-llvm::Value *LogErrorV(const char *Str) {
-  cout<<Str<<endl;;
-  return nullptr;
+llvm::Value *LogErrorV(const char *Str)
+{
+    cout << Str << endl;
+    return nullptr;
+}
+
+llvm::Value *Prohead::CodeGen()
+{
+    return nullptr;
+}
+
+llvm::Value *Expressions::CodeGen()
+{
+    return nullptr;
+}
+llvm::Value *Classbody::CodeGen()
+{
+    return nullptr;
+}
+
+llvm::Value *Class::CodeGen()
+{
+    return nullptr;
+}
+
+llvm::Value *Proclass::CodeGen()
+{
+    return nullptr;
+}
+
+llvm::Value *Evaluate::CodeGen()
+{
+    return nullptr;
+}
+
+llvm::Value *Number::CodeGen()
+{
+    return llvm::ConstantFP::get(TheContext, llvm::APFloat(this->val));
+}
+
+llvm::Value *Object::CodeGen()
+{
+    return nullptr;
+}
+
+llvm::Value *Use::CodeGen()
+{
+    return nullptr;
+}
+
+llvm::Value *Operation::CodeGen()
+{
+    llvm::Value *lv = this->left->CodeGen();
+    llvm::Value *rv = this->right->CodeGen();
+
+    switch (this->op)
+    {
+    case '+':
+        return Builder.CreateFAdd(lv, rv, "addtmp");
+    case '-':
+        return Builder.CreateFSub(lv, rv, "subtmp");
+    case '*':
+        return Builder.CreateFMul(lv, rv, "multmp");
+    case '/':
+        return Builder.CreateFDiv(lv, rv, "divtmp");
+    default:
+        return LogErrorV("invalid binary operator");
+    }
+}
+
+llvm::Value *Parameter::CodeGen()
+{
+    return nullptr;
+}
+
+llvm::Value *Parameters::CodeGen()
+{
+    return nullptr;
+}
+
+llvm::Value *Function::CodeGen()
+{
+    return nullptr;
 }
--- src/tree.cc
+++ src/tree.cc
@@ -2,7 +2,7 @@
  * @Author: Outsider
  * @Date: 2022-10-23 20:53:44
  * @LastEditors: Outsider
- * @LastEditTime: 2022-11-02 16:41:18
+ * @LastEditTime: 2022-11-22 15:58:55
  * @Description: In User Settings Edit
  * @FilePath: /compiler/src/tree.cc
  */
@@ -19,10 +19,6 @@ Expression::~Expression() {}
 
 Prohead::Prohead(int line, Token *using_name) : Expression(line), name(using_name) {}
 Prohead::Prohead(Token *using_name) : name(using_name) {}
-llvm::Value *Prohead::CodeGen()
-{
-    return nullptr;
-}
 void Prohead::print(int level)
 {
     std::string str = std::string(level, ' ');
@@ -40,10 +36,6 @@ Expressions *Expressions::add(Expression *exp)
     explist.push_back(exp);
     return this;
 }
-llvm::Value *Expressions::CodeGen()
-{
-    return nullptr;
-}
 void Expressions::print(int level)
 {
     std::string str = std::string(level, ' ');
@@ -57,10 +49,7 @@ void Expressions::print(int level)
 Expressions::~Expressions() {}
 
 Classbody::Classbody(int line, Expressions *explist) : Expression(line), explist(explist) {}
-llvm::Value *Classbody::CodeGen()
-{
-    return nullptr;
-}
+
 void Classbody::print(int level)
 {
     std::string str = std::string(level, ' ');
@@ -72,10 +61,7 @@ Classbody::~Classbody() {}
 Class ::Class(int line, Token *token) : Expression(line), name(token) {}
 Class ::Class(int line, Token *token, Classbody *body)
     : Expression(line), name(token), classbody(body) {}
-llvm::Value *Class::CodeGen()
-{
-    return nullptr;
-}
+
 void Class::print(int level)
 {
     std::string str = std::string(level, ' ');
@@ -101,10 +87,7 @@ Proclass *Proclass::add(Class *a)
     classes.push_back(a);
     return this;
 }
-llvm::Value *Proclass::CodeGen()
-{
-    return nullptr;
-}
+
 void Proclass::print(int level)
 {
     std::string str = std::string(level, ' ');
@@ -130,10 +113,7 @@ Program::~Program() {}
 
 Evaluate::Evaluate(int line, Token *left, Expression *right)
     : Expression(line), left(left), right(right) {}
-llvm::Value *Evaluate::CodeGen()
-{
-    return nullptr;
-}
+
 void Evaluate::print(int level)
 {
     std::string str = std::string(level, ' ');
@@ -148,10 +128,6 @@ Number::Number(int line, Token *token) : token(token), Expression(line)
     this->val = stold(this->token->str);
 }
 
-llvm::Value *Number::CodeGen()
-{
-    return llvm::ConstantFP::get(TheContext, llvm::APFloat(this->val));
-}
 void Number::print(int level)
 {
     std::string str = std::string(level, ' ');
@@ -161,10 +137,7 @@ void Number::print(int level)
 Number::~Number() {}
 
 Object::Object(int line, Token *token) : token(token), Expression(line) {}
-llvm::Value *Object::CodeGen()
-{
-    return nullptr;
-}
+
 void Object::print(int level)
 {
     std::string str = std::string(level, ' ');
@@ -174,10 +147,7 @@ void Object::print(int level)
 Object::~Object() {}
 
 Use::Use(int line, Expression *exp) : Expression(line), exp(exp) {}
-llvm::Value *Use::CodeGen()
-{
-    return nullptr;
-}
+
 void Use::print(int level)
 {
     std::string str = std::string(level, ' ');
@@ -189,25 +159,7 @@ Use::~Use() {}
 
 Operation::Operation(int line, Expression *left, char op, Expression *right)
     : Expression(line), left(left), op(op), right(right) {}
-llvm::Value *Operation::CodeGen()
-{
-    llvm::Value *lv = this->left->CodeGen();
-    llvm::Value *rv = this->right->CodeGen();
 
-    switch (this->op)
-    {
-    case '+':
-        return Builder.CreateFAdd(lv, rv, "addtmp");
-    case '-':
-        return Builder.CreateFSub(lv, rv, "subtmp");
-    case '*':
-        return Builder.CreateFMul(lv, rv, "multmp");
-    case '/':
-        return Builder.CreateFDiv(lv, rv, "divtmp");
-    default:
-        return LogErrorV("invalid binary operator");
-    }
-}
 void Operation::print(int level)
 {
     std::string str = std::string(level, ' ');
@@ -219,10 +171,7 @@ void Operation::print(int level)
 Operation::~Operation() {}
 
 Parameter::Parameter() {}
-llvm::Value *Parameter::CodeGen()
-{
-    return nullptr;
-}
+
 void Parameter::print(int level)
 {
     std::string str = std::string(level, ' ');
@@ -231,10 +180,7 @@ void Parameter::print(int level)
 Parameter::~Parameter() {}
 
 Parameters::Parameters() {}
-llvm::Value *Parameters::CodeGen()
-{
-    return nullptr;
-}
+
 void Parameters::print(int level)
 {
     std::string str = std::string(level, ' ');
@@ -243,10 +189,7 @@ void Parameters::print(int level)
 Parameters::~Parameters() {}
 
 Function::Function() {}
-llvm::Value *Function::CodeGen()
-{
-    return nullptr;
-}
+
 void Function::print(int level)
 {
     std::string str = std::string(level, ' ');
